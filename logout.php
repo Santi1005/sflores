ed27981eb5b4cdd713942a8781706ca50a215516
Sflores <israelsantiagofloresarias@gmail.com>
2026-05-08 09:31:11 -0600

    Actualizacion COOKIES

--- logout.php
+++ logout.php
@@ -2,6 +2,13 @@
 session_start();
 session_unset();    // Elimina las variables
 session_destroy();  // Destruye la sesión
+setcookie("id_usuario", "", time() - 3600, "/");
+header("Location: index.php");
+exit();
+?><?php
+session_start();
+session_unset();    // Elimina las variables
+session_destroy();  // Destruye la sesión
 header("Location: index.php");
 exit();
 ?>
